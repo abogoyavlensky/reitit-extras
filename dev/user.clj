00ca1ca83cfa4712050af623a513af41f4effcce
Andrey Bogoyavlenskiy <abogoyavlensky@gmail.com>
2025-02-12 23:49:41 +0000

    Add user env

---
+++ dev/user.clj
@@ -0,0 +1,18 @@
+(ns user
+  (:require [clojure.tools.namespace.repl :as repl]
+            [clojure.test :as test]))
+
+(repl/set-refresh-dirs "dev" "src" "test")
+
+
+(defn reset
+  "Reload changed namespaces."
+  []
+  (repl/refresh))
+
+
+(defn run-all-tests
+  "Run all tests."
+  []
+  (reset)
+  (test/run-all-tests #"reitit-extras.*-test"))
