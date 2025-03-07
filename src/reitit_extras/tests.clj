4a7ba1fcb1fc6f781562697c4977fcfa37e640fa
Andrey Bogoyavlenskiy <abogoyavlensky@gmail.com>
2025-03-07 01:08:48 +0000

    Add getting server url fn for tests

---
+++ src/reitit_extras/tests.clj
@@ -0,0 +1,16 @@
+(ns reitit-extras.tests)
+
+(defn get-server-url
+  "Return full url from jetty server object.
+  * server - jetty server object
+  * env - :host or :container
+  :host - localhost
+  :container - testcontainers internal host"
+  ([server]
+   (get-server-url server :host))
+  ([server env]
+   (let [base-url (case env
+                    :host "http://localhost"
+                    :container "http://host.testcontainers.internal:")
+         port (.getLocalPort (first (.getConnectors server)))]
+     (str base-url port))))
