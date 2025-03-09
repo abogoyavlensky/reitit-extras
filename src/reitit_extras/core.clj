12796284151152dd81021fc2a7dbff631e50bfd5
Andrey Bogoyavlenskiy <abogoyavlensky@gmail.com>
2025-03-09 02:38:42 +0000

    Add default server for rendering html

--- src/reitit_extras/core.clj
+++ src/reitit_extras/core.clj
@@ -3,12 +3,27 @@
   (:require [clojure.string :as str]
             [clojure.tools.logging :as log]
             [hiccup2.core :as hiccup]
+            [muuntaja.core :as muuntaja-core]
+            [reitit.coercion.malli :as coercion-malli]
             [reitit.core :as reitit]
+            [reitit.dev.pretty :as pretty]
             [reitit.ring :as ring]
+            [reitit.ring.coercion :as ring-coercion]
             [reitit.ring.middleware.exception :as exception]
+            [reitit.ring.middleware.multipart :as ring-multipart]
+            [reitit.ring.middleware.muuntaja :as muuntaja]
+            [reitit.ring.middleware.parameters :as ring-parameters]
+            [ring.middleware.anti-forgery :as anti-forgery]
+            [ring.middleware.content-type :as content-type]
+            [ring.middleware.cookies :as ring-cookies]
+            [ring.middleware.default-charset :as default-charset]
             [ring.middleware.gzip :as gzip]
+            [ring.middleware.not-modified :as not-modified]
+            [ring.middleware.session :as ring-session]
+            [ring.middleware.session.cookie :as ring-session-cookie]
+            [ring.middleware.x-headers :as x-headers]
             [ring.util.response :as response])
-  (:import [java.security MessageDigest]))
+  (:import (java.security MessageDigest)))
 
 ; Middlewares
 
@@ -36,40 +51,6 @@
 (defn string->16-byte-array [s]
   (let [digest (MessageDigest/getInstance "MD5")]
     (.digest digest (.getBytes s "UTF-8"))))
-
-; Handlers
-
-(def ^:private DEFAULT-CACHE-30D "public,max-age=2592000,immutable")
-
-(defn create-resource-handler-cached
-  "Return resource handler with optional Cache-Control header."
-  [{:keys [cached? cache-control]
-    :or {cached? false
-         cache-control DEFAULT-CACHE-30D}
-    :as opts}]
-  (letfn [(resource-response-cached-fn
-            ([path]
-             (resource-response-cached-fn path {}))
-            ([path options]
-             (-> (response/resource-response path options)
-                 (response/header "Cache-Control" cache-control))))]
-    (let [response-fn (if cached?
-                        resource-response-cached-fn
-                        response/resource-response)]
-      (-> response-fn
-          (ring/-create-file-or-resource-handler opts)
-          (gzip/wrap-gzip)))))
-
-(defn render-html
-  "Render hiccup content as HTML response.
-
-  Note: hiccup/hiccup is not a direct dependency of this library."
-  [content]
-  (-> content
-      (hiccup/html)
-      (str)
-      (response/response)
-      (response/header "Content-Type" "text/html")))
 
 ; URLs
 
@@ -113,3 +94,100 @@
 
        ; print stack-traces for all exceptions
        ::exception/wrap wrap-exception})))
+
+; Handlers
+
+(defn csrf-token
+  []
+  [:input {:type "hidden"
+           :name "__anti-forgery-token"
+           :id "__anti-forgery-token"
+           :value (force anti-forgery/*anti-forgery-token*)}])
+
+(def ^:private DEFAULT-CACHE-30D "public,max-age=2592000,immutable")
+
+(defn create-resource-handler-cached
+  "Return resource handler with optional Cache-Control header."
+  [{:keys [cached? cache-control]
+    :or {cached? false
+         cache-control DEFAULT-CACHE-30D}
+    :as opts}]
+  (letfn [(resource-response-cached-fn
+            ([path]
+             (resource-response-cached-fn path {}))
+            ([path options]
+             (-> (response/resource-response path options)
+                 (response/header "Cache-Control" cache-control))))]
+    (let [response-fn (if cached?
+                        resource-response-cached-fn
+                        response/resource-response)]
+      (-> response-fn
+          (ring/-create-file-or-resource-handler opts)
+          (gzip/wrap-gzip)))))
+
+(defn render-html
+  "Render hiccup content as HTML response.
+
+  Note: hiccup/hiccup is not a direct dependency of this library."
+  [content]
+  (-> content
+      (hiccup/html)
+      (str)
+      (response/response)
+      (response/header "Content-Type" "text/html")))
+
+(defn handler-ssr
+  "Return main application handler for server-side rendering."
+  [{:keys [routes default-handlers session-store]} {:keys [options]
+                                                    :as context}]
+  (let [session-store (or session-store
+                          (ring-session-cookie/cookie-store
+                            {:key (string->16-byte-array (:session-secret-key options))}))]
+    (ring/ring-handler
+      (ring/router
+        routes
+        {:exception pretty/exception
+         :data {:muuntaja muuntaja-core/instance
+                :coercion coercion-malli/coercion
+                :middleware [[x-headers/wrap-content-type-options :nosniff]
+                             [x-headers/wrap-frame-options :sameorigin]
+                             not-modified/wrap-not-modified
+                             content-type/wrap-content-type
+                             [default-charset/wrap-default-charset "utf-8"]
+                             ring-cookies/wrap-cookies
+                             [ring-session/wrap-session
+                              {:cookie-attrs {:secure true
+                                              :http-only true}
+                               :flash true
+                               :store session-store}]
+                             ; add handler options to request
+                             [wrap-context context]
+                             ; parse any request parameters
+                             ring-parameters/parameters-middleware
+                             ; send files
+                             ring-multipart/multipart-middleware
+                             ; negotiate request and response
+                             muuntaja/format-middleware
+                             ; Check CSRF token
+                             ; add call (linkboard.components/csrf-token) to a form
+                             anti-forgery/wrap-anti-forgery
+                             ; handle exceptions
+                             exception-middleware
+                             ; coerce request and response to spec
+                             ring-coercion/coerce-exceptions-middleware
+                             ring-coercion/coerce-request-middleware
+                             ring-coercion/coerce-response-middleware]}})
+      (ring/routes
+        (create-resource-handler-cached {:path "/assets/"
+                                         :cached? (:cache-assets? options)
+                                         :cache-control (:cache-control options)})
+        (ring/redirect-trailing-slash-handler)
+        (ring/create-default-handler default-handlers)))))
+
+(defn get-handler-ssr
+  [handler-config {:keys [options]
+                   :as context}]
+  (let [handler-fn #(handler-ssr handler-config context)]
+    (if (:auto-reload? options)
+      (wrap-reload handler-fn)
+      (handler-fn))))
